0424a922abf65bf388fd21f252626bdae4b8bf98
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-01-26 22:13:59 -0400

    Pagination

--- Client/src/main/java/br/net/mfs/Client/services/ClientService.java
+++ Client/src/main/java/br/net/mfs/Client/services/ClientService.java
@@ -1,14 +1,14 @@
 package br.net.mfs.Client.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,11 +25,11 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		List<ClientDTO> listDto = list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 
-		return listDto;
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+
 	}
 
 	@Transactional(readOnly = true)
@@ -71,24 +71,22 @@ public class ClientService {
 			//
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
-			
-			throw new ResourceNotFoundException("Id not found : "+id ) ;
-	
+
+			throw new ResourceNotFoundException("Id not found : " + id);
+
 		}
 
 	}
 
 	public void delete(Long id) {
 		try {
-			
-		
-		repository.deleteById(id);
-		} 
-		catch ( EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id not found : "+id );
-			
-		} catch (DataIntegrityViolationException e ) {
-			throw new DatabaseException( "Integrity violation" ) ;
+
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found : " + id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 
